300ea56b55a298d2e3665b5aa172f352ed795675
Robert Prellwitz <robprellwitz@uwalumni.com>
2020-10-20 19:48:20 -0500

    put backpack class into seperate file

---
+++ SodaMachine/BackPack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SodaMachine
+{
+    public class BackPack : Customer
+    {
+        public List<Can> sodas;
+
+        public BackPack()
+        {
+            sodas = new List<Can>();
+        }
+
+    }
+}
