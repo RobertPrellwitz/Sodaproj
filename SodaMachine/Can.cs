92eacd069a30dbf547897efcca79a80293521236
Robert Prellwitz <robprellwitz@uwalumni.com>
2020-10-20 19:49:12 -0500

    child classes of can class

--- SodaMachine/Can.cs
+++ SodaMachine/Can.cs
@@ -3,8 +3,43 @@ namespace SodaMachine
 {
     public abstract class Can
     {
+        protected double cost;
+
+        public double Cost
+        {
+            get
+            {
+                return cost;
+            }
+        }
+        public string name;
+
         public Can()
         {
+        }
+    }
+    public class Cola : Can
+    {
+        public Cola()
+        {
+            name = "Cola";
+            cost = 0.35;
+        }
+    }
+    public class RootBeer : Can
+    {
+        public RootBeer()
+        {
+            name = "Root Beer";
+            cost = 0.60;
+        }
+    }
+    public class Orange : Can
+    {
+        public Orange()
+        {
+            name = "Orange";
+            cost = 0.06;
         }
     }
 }
