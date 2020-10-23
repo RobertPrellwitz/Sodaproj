f4047e7f80f4bffa6b3b0402f316a057d7dad126
Rob Prellwitz <robprellwitz@uwalumni.com>
2020-10-22 19:11:13 -0500

    cleaned up main simulation method - deleted old scratch pad method

--- SodaMachine/Simulation.cs
+++ SodaMachine/Simulation.cs
@@ -16,10 +16,7 @@ namespace SodaMachine
         public Simulation()
         {
             customer = new Customer();
-            // INSTANTIATE customer
             sodaMachine = new SodaMachine();
-            //BackPack patagoinia = new BackPack();
-            //Wallet trifold = new Wallet();
         }
 
         public void BuySoda()
@@ -28,7 +25,7 @@ namespace SodaMachine
             do
             {
                 UserInterface.Hello();
-                //sodaMachine.CurrentInventory();
+               
                 int buy = UserInterface.CustomerSelectionCheck(sodaMachine);
 
                 customer.ChooseSoda(sodaMachine.inventory, buy);
@@ -43,6 +40,7 @@ namespace SodaMachine
                 sodaMachine.CurrentInventory();
                 sodaMachine.RegisterTotal();
                 customer.pack.BackPackContents();
+                
                 Console.WriteLine("Would you like to use the soda machine again?\n1 = yes \n2 = no");
                 temp = UserInterface.runCheck();
 
@@ -56,56 +54,7 @@ namespace SodaMachine
         public void Purchase()
         {
             
-            // BackPack patagoinia = new BackPack();
-            //Wallet trifold = new Wallet();
-
-            Console.WriteLine($"you have {customer.walletobj.totalWallet} in your wallet");
-
-            customer.walletobj.CurrentChange();
-            Console.WriteLine($"\nyou have the following in your backpack");
-           
-            customer.pack.BackPackContents();
-            //machine.MachineChange();
-            //machine.MachineStock();
-            sodaMachine.CurrentInventory();
-
-           
-
-            //customer.ChooseSoda(sodaMachine.inventory);
-
-            customer.walletobj.CurrentChange();
-
-            Console.WriteLine("\nPlease select which coins to use:\n1 penny \n2 nickel \n3 dime \n4 quarter");
-            customer.MakePayment(customer.walletobj.coins);
-            
-            Console.WriteLine("you put the following in the machine");
-            double total = 0;
-            for (int i = 0; i < customer.payment.Count; i++)
-            {
-                Console.WriteLine(customer.payment[i].name);
-                total = total + customer.payment[i].Value;
-            }
-            Console.WriteLine($"which totals :{total}");
-           
-
-            sodaMachine.ProcessTransaction(customer);
-
-            sodaMachine.CurrentInventory();            
-
-            customer.AddToBackPack();
-
-            //sodaMachine.TotalChange();
-
-            //Console.WriteLine($"Your total change is: {Math.Round(sodaMachine.totalChange,2)}");
-
-
-            //customer.AddToWallet(sodaMachine.changeCoins);
-
-
-
-            sodaMachine.CurrentInventory();
-            customer.walletobj.CurrentChange();
-            //customer.BackPackContents();
+       
         }
                 
 
